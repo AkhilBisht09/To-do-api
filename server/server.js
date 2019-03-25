716b837c21a2b689527288b98f9f11ef12632cf2
Akhil Bisht <AkhilBisht09@gmail.com>
2019-03-25 10:30:05 +0530

    resource query

--- server/server.js
+++ server/server.js
@@ -17,9 +17,31 @@ todo.save().then((docs)=>{
     res.send(docs);
 },(err)=>{
     res.status(400).send(err);
-})
 });
-
+});
+app.get('/todos',(req,res)=>{
+    Todo.find().then((Todos)=>{
+    res.send({Todos});
+    },(e)=>{
+     res.status(400).send(e);
+    });
+});
+app.get('/todos/:id',(req,res)=>{
+    
+    var id=req.params.id;
+    if(mongoose.Types.ObjectId.isValid(id)){
+    Todo.findById({_id:id}).then((todos)=>{
+        if(!todos)
+       return res.status(404).send();
+       res.send({todos});
+    }).catch((err)=>{
+        res.send("Error 400");
+    });
+    }
+    else{
+        res.status(404).send();
+    }
+});
 app.listen(3000,()=>{
     console.log("started on port 3000");
 });
